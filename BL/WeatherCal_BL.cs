65eab7859b4857ffbda415bbf2944bc625f72b33
gmalkiel <galit2944@gmail.com>
2022-10-20 18:38:53 +0300

    1838

--- BL/WeatherCal_BL.cs
+++ BL/WeatherCal_BL.cs
@@ -9,9 +9,23 @@ namespace BL
 {
     public class WeatherCal_BL
     {
-        public WeatherParams GetWeather(string City)
+        //public WeatherParams GetWeather(string City)
+        //{
+        //    DAL.WeatherCalAdapter dal=new DAL.WeatherCalAdapter();
+        //    string WeatherJson = dal.GetTodayWeather(City);
+        //    WeatherParams weather = new WeatherParams();
+        //    if (WeatherJson != null)
+        //    {
+        //        Root myWeather = JsonConvert.DeserializeObject<Root>(WeatherJson);
+        //        weather.City = myWeather.name;
+        //        weather.Degrees = myWeather.main.temp;
+        //        weather.ForeCast = myWeather.weather[0].description;
+        //    }
+        //    return weather;
+        //}
+        public double GetWeather(string City)
         {
-            DAL.WeatherCalAdapter dal=new DAL.WeatherCalAdapter();
+            DAL.WeatherCalAdapter dal = new DAL.WeatherCalAdapter();
             string WeatherJson = dal.GetTodayWeather(City);
             WeatherParams weather = new WeatherParams();
             if (WeatherJson != null)
@@ -21,7 +35,7 @@ namespace BL
                 weather.Degrees = myWeather.main.temp;
                 weather.ForeCast = myWeather.weather[0].description;
             }
-            return weather;
+            return weather.Degrees;
         }
     }
 }
